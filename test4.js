d6b3bcd46dc4bdf7e4beef9951363fcebf4e68d2
amotzk4 <amotzk4@gmail.com>
2016-02-15 22:19:36 +0200

    Create test4.js

---
+++ test4.js
@@ -0,0 +1,31 @@
+var http = require('http');
+
+var options = {
+  host: '127.0.0.1',
+  port: 3000,
+  path: '/getCurrentGruopMsg/?group_id=0'
+};
+
+callback = function(response) {
+  var str = '';
+
+  //another chunk of data has been recieved, so append it to `str`
+  response.on('data', function (chunk) {
+    str += chunk;
+  });
+
+  //the whole response has been recieved, so we just print it out here
+  response.on('end', function () {
+	var ans='"group_id 0 user_id 0 msg eli time 22:00 "';
+	if(ans==str){    
+	console.log('good test');
+	}
+	else{
+	console.log('test feild');
+	console.log('ans: '+ans);
+	console.log('str: '+ str);
+	}
+  });
+}
+
+http.request(options, callback).end();
